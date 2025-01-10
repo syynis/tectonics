469df291ec9f82579fc695de2f8bdc0204ce4383
Syniis <fynn.c.lohren@gmail.com>
2025-01-10 16:31:40 +0100

    Fix off by one

--- src/poisson.rs
+++ src/poisson.rs
@@ -1,4 +1,4 @@
-use std::f32::consts::{SQRT_2, TAU};
+use std::f32::consts::{PI, SQRT_2, TAU};
 
 use rand::Rng;
 use vek::{Aabr, Vec2};
@@ -41,10 +41,14 @@ impl PoissonSampler {
         // Init grid
         let mut grid = vec![None; size_scaled.x as usize * size_scaled.y as usize];
 
+        let insert_grid = |p: Vec2<f32>, v: usize| {
+            let grid_idx: Vec2<i32> = size_scaled * ((*p - min) / size).as_();
+            grid[grid_idx.x as usize * size_scaled.y as usize + grid_idx.y as usize] = Some(v);
+        };
+
         // Which cells occupied by existing points already
         for (p_idx, p) in self.points.iter().enumerate() {
-            let grid_idx: Vec2<i32> = size_scaled * ((*p - min) / size).as_();
-            grid[grid_idx.x as usize * size_scaled.y as usize + grid_idx.y as usize] = Some(p_idx);
+            insert_grid(p, p_idx);
         }
 
         // Closest existing point near our preferred point
@@ -88,13 +92,80 @@ impl PoissonSampler {
 
             if free {
                 self.points.push(npos);
-                let grid_idx: Vec2<i32> = size_scaled * ((npos - min) / size).as_();
-                grid[grid_idx.x as usize * size_scaled.y as usize + grid_idx.y as usize] =
-                    Some(self.points.len());
+                insert_grid(npos, self.points.len() - 1);
                 res = Some(npos);
                 break;
             }
         }
         return res;
+    }
+
+    pub fn sample_multiple(&mut self, rng: &mut impl Rng, area: Option<Aabr<f32>>, k: u64) {
+        // No points
+        // Sample area
+        let area = area.unwrap_or(Aabr {
+            min: Vec2::broadcast(0.0),
+            max: self.dimension,
+        });
+        let (min, max) = (area.min, area.max);
+        let size = max - min;
+        let radius = (size.magnitude_squared() / PI / k as f32).sqrt();
+        // Cell size
+        let length = radius / SQRT_2;
+        // Sample area scaled by cell size
+        let size_scaled: Vec2<i32> = (size / length).ceil().as_();
+        // Init grid
+        let mut grid = vec![None; size_scaled.x as usize * size_scaled.y as usize];
+        let insert_grid = |p: Vec2<f32>, v: usize| {
+            let grid_idx: Vec2<i32> = size_scaled * ((*p - min) / size).as_();
+            grid[grid_idx.x as usize * size_scaled.y as usize + grid_idx.y as usize] = Some(v);
+        };
+
+        if self.points.is_empty() {
+            self.points.push(area.center());
+        } else {
+            // Which cells occupied by existing points already
+            for (p_idx, p) in self.points.iter().enumerate() {
+                let grid_idx: Vec2<i32> = size_scaled * ((*p - min) / size).as_();
+                grid[grid_idx.x as usize * size_scaled.y as usize + grid_idx.y as usize] =
+                    Some(p_idx);
+            }
+        }
+
+        // Attempt to find cell next to target which is not occupied
+        for _ in 0..attempts {
+            let nr = rng.gen_range(radius..2.0 * radius);
+            let nt = rng.gen_range(0.0..TAU);
+
+            let npos = target + nr * Vec2::new(nt.cos(), nt.sin());
+            let ind: Vec2<i32> = size_scaled * ((npos - min) / size).as_();
+
+            let mut free = true;
+
+            for i in ind.x - 2..=ind.x + 2 {
+                for j in ind.y - 2..=ind.y + 2 {
+                    if i < 0 || i >= size_scaled.x || j < 0 || j >= size_scaled.y {
+                        continue;
+                    }
+                    if let Some(e) = grid[(i * size_scaled.y + j) as usize] {
+                        if self.points[e].distance_squared(npos) < radius.powi(2) {
+                            free = false;
+                            break;
+                        }
+                    }
+                }
+                if !free {
+                    break;
+                }
+            }
+
+            if free {
+                self.points.push(npos);
+                let grid_idx: Vec2<i32> = size_scaled * ((npos - min) / size).as_();
+                grid[grid_idx.x as usize * size_scaled.y as usize + grid_idx.y as usize] =
+                    Some(self.points.len() - 1);
+                break;
+            }
+        }
     }
 }
